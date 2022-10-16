59d4be52bcc9de22641907969a0d80beabf3f26a
Marcell Rocha Pena <proartcarimbos@gmail.com.br>
2022-10-16 18:11:42 -0300

    refatorando codigo linha 7 Arquivo /src/services/validations/validationsInputValues.js

--- src/services/validations/validationsInputValues.js
+++ src/services/validations/validationsInputValues.js
@@ -4,7 +4,11 @@ const { idSchema, addProductSchema, insertSaleSchema } = require('./joiSchemas')
 
 const validateId = (id) => {
   const { error } = idSchema.validate(id);
-  if (error) return { status: statusCode.NOT_FOUND, message: errorMessages.notFoundData('Product') };
+  if (error) {
+    return {
+      status: statusCode.NOT_FOUND, message: errorMessages.notFoundData('Product'),
+    };
+  }
   return { status: null, message: '' };
 };
 
